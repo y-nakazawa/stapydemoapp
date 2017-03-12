8af54646393b3011340f5988c2423bf22cf0568a
y_nakazawa <y.nakazawa@jsl.co.jp>
2017-03-12 17:38:28 +0900

    bookのCRUD実装

--- cms/urls.py
+++ cms/urls.py
@@ -5,7 +5,7 @@ from cms.views import BookList
 urlpatterns = [
     # 書籍
     url(r'^book/$', BookList.as_view(), name='book_list'),   # 一覧
-#    url(r'^book/add/$', views.book_edit, name='book_add'),  # 登録
-#    url(r'^book/mod/(?P<book_id>\d+)/$', views.book_edit, name='book_mod'),  # 修正
-#    url(r'^book/del/(?P<book_id>\d+)/$', views.book_del, name='book_del'),   # 削除
+    url(r'^book/add/$', views.book_edit, name='book_add'),  # 登録
+    url(r'^book/mod/(?P<book_id>\d+)/$', views.book_edit, name='book_mod'),  # 修正
+    url(r'^book/del/(?P<book_id>\d+)/$', views.book_del, name='book_del'),   # 削除
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, render_to_response
+from django.template import RequestContext
 from django.views.generic import ListView
 
+from cms.forms import BookForm
 from cms.models import Book
 
 
@@ -14,3 +16,27 @@ class BookList(ListView):
         self.object_list = Book.objects.all().order_by('id')  # 書籍を全件取得
         context = self.get_context_data(object_list=self.object_list)
         return self.render_to_response(context)
+
+
+def book_edit(request, book_id=None):
+    """書籍の編集"""
+    if book_id:  # book_id が指定されている (修正時)
+        book = get_object_or_404(Book, pk=book_id)
+    else:  # book_id が指定されていない (追加時)
+        book = Book()
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)  # POST された request データからフォームを作成
+        if form.is_valid():  # フォームのバリデーション
+            form.save()
+            return redirect('cms:book_list')
+    else:  # GET の時
+        form = BookForm(instance=book)  # book インスタンスからフォームを作成
+
+    return render(request, 'cms/book_edit.html', dict(form=form, book_id=book_id))
+
+
+def book_del(request, book_id):
+    """書籍の削除"""
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
